14cc26968d885ee33ea705ec48253294959fff77
dodolebougre <boattidorian97@gmail.com>
2023-11-23 10:48:53 +0100

    Résolution du code

--- app/Homepage/components/Hero.tsx
+++ app/Homepage/components/Hero.tsx
@@ -5,15 +5,14 @@ import Button from "../../components/Button/Button";
 const Hero = () => {
   return (
     <div id="hero">
-      <div className="flex items-center justify-center h-screen lg:mb-11  md:mb-11 sm:mb-11 mb-[-125px] ">
+      <div className="flex items-center justify-center h-screen lg:mb-11  md:mb-11 sm:mb-11 mb-[-120px] ">
         {/* Div pour l'arrière-plan avec le filtre de luminosité */}
         <div
           className="md:bg-fixed bg-center bg-cover object-cover"
           style={{
-            backgroundImage:
-              'url("/assets/image/image site.png")',
-            filter: 'brightness(0.4)',
-            position: 'absolute',
+            backgroundImage: 'url("/assets/image/image site.png")',
+            filter: "brightness(0.4)",
+            position: "absolute",
             top: 0,
             right: 0,
             bottom: 0,
@@ -23,13 +22,16 @@ const Hero = () => {
         ></div>
 
         <div className="p-5 text-white text-center relative z-9 ">
-          <h1 className="text-6xl tracking-tighter sm:text-7xl md:text-7xl lg:text-8xl xl:text-9xl mb-5" style={{ textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)" }}>
+          <h1
+            className="text-6xl tracking-tighter sm:text-7xl md:text-7xl lg:text-8xl xl:text-9xl mb-5"
+            style={{ textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)" }}
+          >
             Transformez vos souvenirs en œuvre d’art
           </h1>
           <p className="max-w-2xl mx-auto text-xl tracking-tighter sm:text-1.5xl md:text-2xl lg:text-3xl mb-9">
             Revivez votre souvenir le plus précieux grâce à une expérience
             unique mêlant <strong>son et image</strong>. Avec Fylia des artistes{" "}
-            <strong>donnent vie</strong> à tous vos récits. 
+            <strong>donnent vie</strong> à tous vos récits.
           </p>
           <Button />
         </div>
