8e01c0b47bc35a296233a0a86766b173464e02be
dodolebougre <boattidorian97@gmail.com>
2023-11-23 02:37:51 +0100

    Résolution du code

--- app/Homepage/components/Cta2.tsx
+++ app/Homepage/components/Cta2.tsx
@@ -11,7 +11,7 @@ const Cta2 = () => {
         ☙ Notre équipe ❧
       </h2>
       <img src="/assets/image/Notreequipe.png" alt="" />
-      <div className="flex flex-col  bg-opacity-10 bg-white rounded p-8 backdrop-blur text-2xl text-center max-w-[400px] lg:max-w-[600px] m-20 shadow">
+      <div className="flex flex-col  bg-opacity-10 bg-white rounded p-4 backdrop-blur text-2xl text-center max-w-[400px] lg:max-w-[600px] m-20 shadow">
         <h2 className=" font-semibold text-2xl md:text-3xl text-center p-5" style={{ textShadow: "1px 1px 5px rgba(0, 0, 0, 0.5)" }}>
           Nous allons sauver des milliers de souvenirs.
         </h2>
